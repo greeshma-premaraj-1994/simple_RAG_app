67429af9f6d59662703a937fd43cbd3e7e676840
Greeshma Premaraj AV <smdlgreeshma@gmail.com>
2024-09-19 17:40:23 +0530

    adding comments,cleaning code

--- backend/main.py
+++ backend/main.py
@@ -15,17 +15,22 @@ from operator import itemgetter
 from pydantic import BaseModel
 from langdetect import detect
 import logging
-# import openai
 import os
+from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings,HuggingFacePipeline
-# from langchain_community.llms import HuggingFacePipeline
 
-# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+logging.basicConfig(level=logging.INFO)
+
+# Load .env file
+load_dotenv()
 
 # Load the OpenAI API key from environment variables
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-logging.basicConfig(level=logging.INFO)
+EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME")
+ARABIC_COLLECTION_NAME = os.getenv("ARABIC_COLLECTION_NAME")
+ENGLISH_COLLECTION_NAME = os.getenv("ENGLISH_COLLECTION_NAME")
+QDRANT_URL = os.getenv("QDRANT_URL")
+LLM_MODEL = os.getenv("LLM_MODEL")
+
 class Config(BaseModel):
     class Config:
         arbitrary_types_allowed = True
@@ -44,32 +49,32 @@ app.add_middleware(
 # Initialize Qdrant client
 qdrant_client = QdrantClient(host="localhost", port=6333)
 
-# Initialize SentenceTransformer for embeddings
-embedding_model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-model = SentenceTransformer(embedding_model_name)
+# embedding
+embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+# # Initialize SentenceTransformer for embeddings
+# model = SentenceTransformer(EMBEDDING_MODEL_NAME)
 
-# Define embedding function
-def embed_texts(texts):
-    return model.encode(texts, convert_to_tensor=True).tolist()
+# # Define embedding function
+# def embed_texts(texts):
+#     return model.encode(texts, convert_to_tensor=True).tolist()
 
 # Create Qdrant vector store from existing collection
 english_vector_store = QdrantVectorStore.from_existing_collection(
     embedding=embeddings,
-    collection_name="english_documents_collection",
-    url="http://localhost:6333"
+    collection_name= ENGLISH_COLLECTION_NAME,
+    url=QDRANT_URL
 )
 arabic_vector_store = QdrantVectorStore.from_existing_collection(
     embedding=embeddings,
-    collection_name="arabic_documents_collection",
-    url="http://localhost:6333"
+    collection_name=ARABIC_COLLECTION_NAME,
+    url=QDRANT_URL
 )
 # Set up memory for conversational retrieval
 memory = ConversationBufferMemory()
 
 # Load GPT-Neo model and tokenizer for text generation
-gpt_neo_model_name = "EleutherAI/gpt-neo-1.3B"
-gpt_neo_model = GPTNeoForCausalLM.from_pretrained(gpt_neo_model_name)
-gpt_neo_tokenizer = AutoTokenizer.from_pretrained(gpt_neo_model_name)
+gpt_neo_model = GPTNeoForCausalLM.from_pretrained(LLM_MODEL)
+gpt_neo_tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL)
 
 # Create a Hugging Face pipeline for text generation
 llm_pipeline = pipeline("text-generation", model=gpt_neo_model, tokenizer=gpt_neo_tokenizer,max_length=256,max_new_tokens=200,truncation=True, num_return_sequences=1)
@@ -81,6 +86,8 @@ llm = HuggingFacePipeline(
 #     result = llm_pipeline(prompt, max_length=max_length, num_return_sequences=1)
 #     return result[0]["generated_text"]
 
+# ------------------------------ Advanced Retrieval Strategies: ----------------------------
+
 # Define the PromptTemplate for combining document context and question
 prompt_template = """You are a helpful assistant. Use the following documents to answer the question:
 
@@ -89,6 +96,7 @@ prompt_template = """You are a helpful assistant. Use the following documents to
 Question: {question}"""
 prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
+#RAG chain for english data retrieval
 rag_chain_1 = (
     {
         "context": itemgetter("question") | english_vector_store.as_retriever(),
@@ -97,22 +105,24 @@ rag_chain_1 = (
     | prompt
     | llm
     | StrOutputParser()
+    | memory
 )
 # rag1=rag_chain_1.invoke({"question": "what is article 1?"})
 
 # logging.info(f"invoked....{rag1}")
 
 
-# Chain
-llm_chain = prompt | llm | StrOutputParser()
+#RAG chain for arabic data retrieval
+llm_chain = prompt | llm | StrOutputParser() | memory
 rag_chain_2 = MultiQueryRetriever(
     retriever=arabic_vector_store.as_retriever(), llm_chain=llm_chain
 )
 # rag2 = rag_chain_2.invoke("عمله الأصلية.")
 # print("len....",len(rag2))
 
+# -------------------------------------------Routing Strategy: -------------------
 
-
+#routing strategy
 def route(info):
     language = detect(info["question"])
     if language == 'en':
@@ -122,6 +132,9 @@ def route(info):
     else:
         return "I am sorry, I am not allowed to answer in this language."
 
+
+# --------------------------------------Streaming Responses: -------------------------
+# ---------------------------------------Message History and Session Management:-------------------
 
 # WebSocket endpoint for real-time query processing
 @app.websocket("/ws")
